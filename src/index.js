b87a6b34a1748acb672b10407637b2226973314c
alizabhattarai <alizabi2000@gmail.com>
2020-08-04 10:34:19 -0500

    third commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let one ="http://anapioficeandfire.com/api/characters/16";
+let two = "http://www.anapioficeandfire.com/api/houses/378";
+let three = "http://www.anapioficeandfire.com/api/houses/229";
+let four ="http://www.anapioficeandfire.com/api/houses/17";
+let five ="http://www.anapioficeandfire.com/api/characters/901";
+let six = "http://www.anapioficeandfire.com/api/houses/362";
+let seven = "http://www.anapioficeandfire.com/api/characters/232";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const requestOne = axios.get(one);
+const requestTwo = axios.get(two);
+const requestThree = axios.get(three);
+const requestFour = axios.get(four);
+const requestFive = axios.get(five);
+const requestSix = axios.get(six);
+const requestSeven = axios.get(seven);
+
+axios
+    .all ([requestOne, requestTwo, requestThree, requestFour, requestFive, requestSix, requestSeven])
+    .then (
+        axios.spread ((...responses ) =>{
+            const responseOne = responses [0];
+            const responseTwo = responses [1];
+            const responseThree = responses [2];
+            const responseFour = responses [3];
+            const responseFive = responses [4];
+            const responseSix = responses [5];
+            const responseSeven = responses [6];
+
+            console.log( responseOne, responseTwo, responseThree, responseFour, responseFive, responseSix, responseSeven);
+
+        })
+    )
+
+.catch(errors => {
+console.error (errors);
+}); 
